36831dddf56806ed5bc42c81e3ac5dc9b01562ec
Yosua Kristianto <yosuakristianto754@gmail.com>
2021-07-25 23:30:41 +0700

    Added Home

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Facade\BaseResponse;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\View\View;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function slash(): Factory|View|Application
+    {
+        return view('home');
+    }
 }
